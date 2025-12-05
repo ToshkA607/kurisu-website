042bd96af8e70c4311ccd9e3f196fd6dd604061c
ToshkA607 <artemtaskaev411@gmail.com>
2025-12-05 23:02:41 +0700

    Update firebase-config.mjs

--- firebase-config.mjs
+++ firebase-config.mjs
@@ -1,7 +1,7 @@
 // firebase-config.mjs
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore, Timestamp } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDBCjzCv1mAAjhV9_ljTXZ7Fn8Ix1_vU-k",
@@ -15,4 +15,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export { Timestamp };
