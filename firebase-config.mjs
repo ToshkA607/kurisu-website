376aae79560f919022e6b8ac5f521adbfa28ecff
ToshkA607 <artemtaskaev411@gmail.com>
2025-12-05 23:54:14 +0700

    Update firebase-config.mjs

--- firebase-config.mjs
+++ firebase-config.mjs
@@ -1,17 +1,1 @@
-// firebase-config.mjs
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDBCjzCv1mAAjhV9_ljTXZ7Fn8Ix1_vU-k",
-  authDomain: "kurisu-lab.firebaseapp.com",
-  projectId: "kurisu-lab",
-  storageBucket: "kurisu-lab.firebasestorage.app",
-  messagingSenderId: "754858147142",
-  appId: "1:754858147142:web:1f03a6f17c442132297947"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
